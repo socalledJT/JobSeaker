c80631e0d553a6e3d5776b4e6c92943ad96d90de
Johnny Tm <36928647+socalledJT@users.noreply.github.com>
2023-11-15 16:22:33 +0100

    Minor bug fixes

--- app/_layout.js
+++ app/_layout.js
@@ -6,7 +6,7 @@ import { useFonts } from "expo-font";
 
 export const unstable_settings = {
   // Ensure any route can link back to `/`
-  initialRouteName: "home",
+  initialRouteName: "index",
 };
 
 const Layout = () => {
